d5bf4fd22ce36b22488744782bcbdc764f864e51
Oscar De La Lanza <oscardelalanza@gmail.com>
2023-01-08 22:03:44 -0800

    refactor: rswag removed

--- app/controllers/api/v1/admin/auth/registrations_controller.rb
+++ app/controllers/api/v1/admin/auth/registrations_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::Admin::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
   before_action :one_user_registered?, only: [:create]
+  before_action :configure_permitted_parameters
 
   protected
 
   def one_user_registered?
     render_error(:forbidden, I18n.t('errors.forbidden')) if User.count >= 1
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name nickname])
   end
 end
