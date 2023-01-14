7e87529201715644863f8ede9b56b256fbd876f7
Oscar De La Lanza <oscardelalanza@gmail.com>
2023-01-13 23:55:53 -0800

    feat: registration controller protection method refactored

--- app/controllers/api/v1/admin/auth/registrations_controller.rb
+++ app/controllers/api/v1/admin/auth/registrations_controller.rb
@@ -4,6 +4,14 @@ class Api::V1::Admin::Auth::RegistrationsController < DeviseTokenAuth::Registrat
   protected
 
   def one_user_registered?
-    render_error(:forbidden, I18n.t('errors.forbidden')) if User.count >= 1
+    render_error(:forbidden, I18n.t('errors.forbidden')) unless registrable?
+  end
+
+  private
+
+  def registrable?
+    return if User.count >= ENV.fetch('ADMIN_USERS_ALLOWED', 1).to_i
+
+    true
   end
 end
