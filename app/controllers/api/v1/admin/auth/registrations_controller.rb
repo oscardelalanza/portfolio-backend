4d3b8baf8c821d9703c5d932f4cec4b67d61e6d4
Oscar De La Lanza <oscardelalanza@gmail.com>
2023-01-14 00:00:13 -0800

    refactor: registration methods moved to a concern

--- app/controllers/api/v1/admin/auth/registrations_controller.rb
+++ app/controllers/api/v1/admin/auth/registrations_controller.rb
@@ -1,17 +1,5 @@
 class Api::V1::Admin::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  include Api::V1::Admin::Auth::Registrations
+
   before_action :one_user_registered?, only: [:create]
-
-  protected
-
-  def one_user_registered?
-    render_error(:forbidden, I18n.t('errors.forbidden')) unless registrable?
-  end
-
-  private
-
-  def registrable?
-    return if User.count >= ENV.fetch('ADMIN_USERS_ALLOWED', 1).to_i
-
-    true
-  end
 end
