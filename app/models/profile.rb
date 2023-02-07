482b0318e4a213656abb99a3b7e7b0dae0087efe
Oscar De La Lanza <oscardelalanza@gmail.com>
2023-02-06 21:25:27 -0800

    revert: profile and profile attributes removed (#8)

--- app/models/profile.rb
+++
@@ -1,6 +0,0 @@
-class Profile < ApplicationRecord
-  has_many :profile_attributes, dependent: :restrict_with_error
-
-  validates :title, presence: true, length: { maximum: 50 }
-  validates :description, allow_blank: true, length: { maximum: 255 }
-end
