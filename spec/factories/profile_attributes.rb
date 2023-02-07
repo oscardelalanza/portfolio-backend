13a68a03189c40fc0c7887024a61bac763ebc258
Oscar De La Lanza <oscardelalanza@gmail.com>
2023-02-06 21:00:07 -0800

    revert: profile and profile attributes removed

--- spec/factories/profile_attributes.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :profile_attribute do
-    profile
-    key { Faker::Internet.slug(glue: '_') }
-    value { Faker::Hipster.sentence }
-    description { Faker::Hipster.paragraph_by_chars(characters: 255) }
-    visible { true }
-  end
-end
--- spec/factories/profiles.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :profile do
-    title { Faker::Coffee.blend_name }
-    description { Faker::Hipster.paragraph_by_chars(characters: 255) }
-  end
-end
